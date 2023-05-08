3dd6d165a487a3eda8523bd82c82b2c7860fcbc8
Panesico <101527876+Panesico@users.noreply.github.com>
2023-05-08 23:19:17 +0000

    Update ft_strjoin.c

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -16,22 +16,18 @@ char	*ft_strjoin(char const *s1, char const *s2)
 {
 	size_t	i;
 	size_t	j;
-	size_t	s1_len;
-	size_t	s2_len;
 	char	*str;
 
-	s1_len = (size_t)ft_strlen(s1);
-	s2_len = (size_t)ft_strlen(s2);
-	str = (char *)malloc((s1_len + s2_len + 1) * sizeof(char));
+	str = (char *)malloc(((ft_strlen(s1) + ft_strlen(s2)) + 1)  * sizeof(char));
 	if (!str)
 		return (0);
-	i = 0;
+	i = -1;
 	j = 0;
-	while (j < s1_len && s1[j])
-		str[i++] = s1[j++];
-	j = 0;
-	while (j < (s1_len + s2_len) && s2[j])
+	if (s1)
+		while (s1[++i] != '\0')
+			str[i] = s1[i];
+	while (s2[j] != '\0')
 		str[i++] = s2[j++];
 	str[i] = 0;
-	return (str);
+	return (free(s1), str);
 }
